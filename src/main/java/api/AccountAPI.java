1e8e7c07089cfdbacb47890a387ba973b781e6a9
daiweisong <5600199+daiweisong@users.noreply.github.com>
2019-07-17 22:27:13 +0800

    API定义

--- src/main/java/api/AccountAPI.java
+++ src/main/java/api/AccountAPI.java
@@ -1,4 +1,18 @@
 package api;
 
+import model.AccountCommonRequest;
+import model.AccountCommonResult;
+
+/**
+ * 账户记账接口
+ */
 public interface AccountAPI {
+
+
+    /**
+     * 支持复杂传票处理，复杂流水处理
+     * @param accountCommonRequest
+     * @return
+     */
+    public AccountCommonResult doAccount(AccountCommonRequest accountCommonRequest)  ;
 }
